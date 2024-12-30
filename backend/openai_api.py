73ff5a7ed275556d1890c92dac55068948102a28
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2024-12-30 16:40:07 +0100

    Remove redundant code

--- backend/openai_api.py
+++ backend/openai_api.py
@@ -1,9 +1,7 @@
 # Import required libraries
 import os
 from openai import OpenAI
-import openai
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 def generate_playlist_description(prompt):
