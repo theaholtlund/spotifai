3f7a258c937335031968060548e1d379f43335a6
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2024-12-31 13:48:33 +0100

    Minor updates

--- backend/spotify_api.py
+++ backend/spotify_api.py
@@ -1,6 +1,22 @@
 # Import required libraries
+import os
+import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 
-# Load environment variables from environment file
+# Load environment variables
 load_dotenv()
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=os.getenv('SPOTIPY_CLIENT_ID'),
+    client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
+    redirect_uri=os.getenv('SPOTIPY_REDIRECT_URI'),
+    scope=os.getenv('SPOTIPY_SCOPE')
+))
+
+def create_playlist(user_id, name, description):
+    playlist = sp.user_playlist_create(user=user_id, name=name, public=True, description=description)
+    return playlist['id']
+
+def add_tracks_to_playlist(playlist_id, tracks):
+    sp.playlist_add_items(playlist_id, tracks)
